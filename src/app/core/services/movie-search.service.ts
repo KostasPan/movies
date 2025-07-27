d569b37dd29f5213753f370a543c17c2e8b2eaca
KPanop <kpanopoulos.dev@gmail.com>
2025-07-27 22:52:10 +0300

    rating

--- src/app/core/services/movie-search.service.ts
+++ src/app/core/services/movie-search.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { TMDbSearchResponse } from '../models/tmdb-search-response.interface';
 import { TMDbMovieResult } from '../models/tmdb-movie-result.interface';
+import { GuestSession } from '../models/guest-session.interface';
 
 @Injectable({
   providedIn: 'root', // Service is available application-wide
@@ -14,6 +15,8 @@ export class SearchService {
   private readonly API_KEY = environment.apiKey;
   private readonly MOVIE_API_URL = 'https://api.themoviedb.org/3/movie';
   private readonly SEARCH_API_URL = 'https://api.themoviedb.org/3/search/movie';
+  private readonly GUEST_SESSION_URL =
+    'https://api.themoviedb.org/3/authentication/guest_session/new';
   readonly IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w200';
 
   searchMovies(query: string, page: number): Observable<TMDbSearchResponse> {
@@ -27,6 +30,25 @@ export class SearchService {
   getMovieDetails(id: number): Observable<TMDbMovieResult> {
     const params = new HttpParams().set('api_key', this.API_KEY);
     return this.http.get<TMDbMovieResult>(`${this.MOVIE_API_URL}/${id}`, {
+      params,
+    });
+  }
+
+  createGuestSession(): Observable<GuestSession> {
+    const params = new HttpParams().set('api_key', this.API_KEY);
+    return this.http.get<GuestSession>(this.GUEST_SESSION_URL, { params });
+  }
+
+  rateMovie(
+    movieId: number,
+    rating: number,
+    guestSessionId: string
+  ): Observable<any> {
+    const params = new HttpParams()
+      .set('api_key', this.API_KEY)
+      .set('guest_session_id', guestSessionId);
+    const body = { value: rating };
+    return this.http.post(`${this.MOVIE_API_URL}/${movieId}/rating`, body, {
       params,
     });
   }
--- src/app/features/movie-details/movie-details.component.ts
+++ src/app/features/movie-details/movie-details.component.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { Observable } from 'rxjs';
 import { TMDbMovieResult } from '../../core/models/tmdb-movie-result.interface';
 import { SearchService } from '../../core/services/movie-search.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-movie-details',
@@ -18,20 +19,54 @@ import { SearchService } from '../../core/services/movie-search.service';
     MatProgressSpinnerModule,
     MatIconModule,
     MatButtonModule,
+    MatSnackBarModule,
   ],
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.css'],
 })
 export class MovieDetailsComponent implements OnInit {
   movie$!: Observable<TMDbMovieResult>;
+  rating: number = 0;
+  hovered: number = 0;
 
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: { movieId: number },
-    private searchService: SearchService
+    private searchService: SearchService,
+    private snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {
     this.movie$ = this.searchService.getMovieDetails(this.data.movieId);
+  }
+
+  setRating(rating: number): void {
+    this.rating = rating;
+  }
+
+  submitRating(): void {
+    this.searchService.createGuestSession().subscribe({
+      next: (session) => {
+        this.searchService
+          .rateMovie(this.data.movieId, this.rating, session.guest_session_id)
+          .subscribe({
+            next: () => {
+              this.snackBar.open('Rating submitted successfully!', 'Close', {
+                duration: 3000,
+              });
+            },
+            error: () => {
+              this.snackBar.open('Failed to submit rating.', 'Close', {
+                duration: 3000,
+              });
+            },
+          });
+      },
+      error: () => {
+        this.snackBar.open('Failed to create a guest session.', 'Close', {
+          duration: 3000,
+        });
+      },
+    });
   }
 }
