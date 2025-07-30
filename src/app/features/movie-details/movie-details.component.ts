da95e415169feb3e2c1c8505c29ce6b950deb868
KPanop <kpanopoulos.dev@gmail.com>
2025-07-30 11:38:57 +0300

    reusable add to collection component

--- src/app/features/movie-details/movie-details.component.ts
+++ src/app/features/movie-details/movie-details.component.ts
@@ -1,18 +1,17 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { CommonModule, Location } from '@angular/common';
-import { MatDialogModule } from '@angular/material/dialog';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { Observable, finalize } from 'rxjs';
+import { Observable, finalize, switchMap } from 'rxjs';
 import { TMDbMovieResult } from '../../core/models/tmdb-movie-result.interface';
 import { TMDBService } from '../../core/services/tmdb.service';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
-import { MovieCollection } from '../../core/models/movie-collection.interface';
-import { CollectionService } from '../../core/services/collection.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AddToCollectionComponent } from '../search/components/add-to-collection/add-to-collection.component';
 
 @Component({
   selector: 'app-movie-details',
@@ -34,27 +33,25 @@ export class MovieDetailsComponent implements OnInit {
   movie$!: Observable<TMDbMovieResult>;
   rating: number = 0;
   hovered: number = 0;
-  collections: MovieCollection[] = [];
-  selectedCollection: string = '';
-  movie: TMDbMovieResult | null = null;
-  movieId: number = 0;
-
   isSubmittingRating = false;
   submittedRating: number | null = null;
+  private movieId: number = 0;
 
   constructor(
     private TMDBService: TMDBService,
     private snackBar: MatSnackBar,
-    private collectionService: CollectionService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {}
 
   ngOnInit(): void {
-    this.movieId = +this.route.snapshot.params['id'];
-    this.movie$ = this.TMDBService.getMovieDetails(this.movieId);
-    this.movie$.subscribe((movie) => (this.movie = movie));
-    this.collections = this.collectionService.getCollections();
+    this.movie$ = this.route.params.pipe(
+      switchMap((params) => {
+        this.movieId = +params['id'];
+        return this.TMDBService.getMovieDetails(this.movieId);
+      })
+    );
   }
 
   setRating(rating: number): void {
@@ -71,57 +68,36 @@ export class MovieDetailsComponent implements OnInit {
     }
     this.isSubmittingRating = true;
 
-    this.TMDBService.createGuestSession().subscribe({
-      next: (session) => {
-        this.TMDBService.rateMovie(
-          this.movieId,
-          this.rating,
-          session.guest_session_id
-        )
-          .pipe(finalize(() => (this.isSubmittingRating = false)))
-          .subscribe({
-            next: () => {
-              this.submittedRating = this.rating;
-              this.snackBar.open('Rating submitted successfully!', 'Close', {
-                duration: 3000,
-              });
-            },
-            error: () => {
-              this.snackBar.open('Failed to submit rating.', 'Close', {
-                duration: 3000,
-              });
-            },
+    this.TMDBService.createGuestSession()
+      .pipe(
+        switchMap((session) =>
+          this.TMDBService.rateMovie(
+            this.movieId,
+            this.rating,
+            session.guest_session_id
+          )
+        ),
+        finalize(() => (this.isSubmittingRating = false))
+      )
+      .subscribe({
+        next: () => {
+          this.submittedRating = this.rating;
+          this.snackBar.open('Rating submitted successfully!', 'Close', {
+            duration: 3000,
           });
-      },
-      error: () => {
-        this.isSubmittingRating = false;
-        this.snackBar.open('Failed to create a guest session.', 'Close', {
-          duration: 3000,
-        });
-      },
-    });
+        },
+        error: () => {
+          this.snackBar.open('Failed to submit rating.', 'Close', {
+            duration: 3000,
+          });
+        },
+      });
   }
 
-  addToCollection(): void {
-    if (this.selectedCollection && this.movie) {
-      if (
-        this.collectionService.addMovieToCollection(
-          this.selectedCollection,
-          this.movie
-        )
-      ) {
-        this.snackBar.open(
-          `Movie added to ${this.selectedCollection}`,
-          'Close',
-          {
-            duration: 2000,
-          }
-        );
-      } else {
-        this.snackBar.open('Movie is already in this collection.', 'Close', {
-          duration: 2000,
-        });
-      }
-    }
+  openAddToCollectionDialog(movie: TMDbMovieResult): void {
+    this.dialog.open(AddToCollectionComponent, {
+      width: '350px',
+      data: { movies: [movie] },
+    });
   }
 }
